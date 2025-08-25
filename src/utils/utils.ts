f5709b2bf4d0c082e0873a422ed2347a1ef31609
ThePuffin <ballutvincent@gmail.com>
2025-08-25 21:09:09 +0200

    fix capitalize

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,8 +2,8 @@ export const randomNumber = (max) => {
   return Math.floor(Math.random() * (max - 0 + 1) + 0);
 };
 
-export const capitalize = (str) => {
-  if (str.length === 0) return str;
+export const capitalize = (str = '') => {
+  if (str?.length === 0) return str;
   return str.replace(
     /(^\w|\s\w)(\S*)/g,
     (_, m1, m2) => m1.toUpperCase() + m2.toLowerCase(),
